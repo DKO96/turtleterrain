fe6f69664cf45f08f97b6ce39167c0f2b89609f8
DKO96 <danielwjkhoo@gmail.com>
2023-11-19 23:46:42 -0500

    bidirectional A*

--- open3d/bidirectional_a_star.py
+++ open3d/bidirectional_a_star.py
@@ -1,5 +1,7 @@
+import time
 import numpy as np
 import matplotlib.pyplot as plt
+import heapq
 from sklearn.neighbors import NearestNeighbors
 
 class Node:
@@ -10,147 +12,108 @@ class Node:
         self.H = H
         self.F = G + H
 
-    def reset_f(self):
-        self.F = self.G + self.H
+    def __lt__(self, other):
+        return self.F < other.F
 
-def heuristic(node_coordinate, goal):
-    return np.linalg.norm(node_coordinate - goal)
+def heuristic(node, goal):
+    return np.linalg.norm(node - goal)
 
-def gcost(fixed_node, update_node_coordinate):
-    # move from start node to updated node
-    gcost = fixed_node.G + heuristic(fixed_node.coordinate, update_node_coordinate)
+def gCost(fixed_node, update_node):
+    gcost = fixed_node.G + heuristic(fixed_node.coordinate, update_node.coordinate)  
     return gcost
+    
+def reconstructPath(forward_node, backward_node, start_node, goal_node):
+    current, start, start_path = forward_node, start_node, []
+    while current != start_node:
+        start_path.append(current.coordinate) 
+        current = current.parent
+    start_path.append(start.coordinate)
+    start_path.reverse() 
 
-def nearest_neighbour(node, nn, close_coordinate_list):
-    node = node.coordinate.reshape(1, -1)
-    distances, indices = nn.kneighbors(node)
-    neighbours = [nn._fit_X[idx] for idx in indices[0] if nn._fit_X[idx] not in close_coordinate_list]
-    return neighbours
+    current, goal, goal_path = backward_node, goal_node, []
+    while current != goal_node:
+        goal_path.append(current.coordinate) 
+        current = current.parent
+    goal_path.append(goal.coordinate)
 
-def find_node_index(coordinate, node_list):
-    ind = 0
-    for node in node_list:
-        if node.coordinate == coordinate:
-            target_node = node
-            ind = node_list.index(target_node)
-            break
-    return ind
+    return start_path + goal_path[1:]
 
-def find_index_of_array(array_list, array_to_find):
-    for i, array in enumerate(array_list):
-        if np.array_equal(array, array_to_find):
-            return i
-    return -1  # Return -1 if the array is not found
+def neighbourSearch(node, nn, points):
+    distances, indices = nn.kneighbors(node.coordinate.reshape(1,-1))
+    indices = indices.reshape(-1)
+    return indices[indices != np.where((points == node.coordinate).all(axis=1))[0][0]]
 
-def find_path(open_list, close_list, goal, nn):
-    # searching for the path, update open and closed list
-    flag = len(open_list)
-    for i in range(flag):
-        node = open_list[0]
-        open_coordinate_list = [node.coordinate for node in open_list]
-        close_coordinate_list = [node.coordinate for node in close_list]
-        temp = nearest_neighbour(node, nn, close_coordinate_list)
-        for element in temp:
-            if element in close_list:
-                continue
-            elif any(np.array_equal(element, coord) for coord in open_coordinate_list):
-                # if node in open list, update g value
-                ind = find_index_of_array(open_coordinate_list, element)
-                new_g = gcost(node, element)
-                if new_g <= open_list[ind].G:
-                    open_list[ind].G = new_g
-                    open_list[ind].reset_f()
-                    open_list[ind].parent = node
-            else:   # new coordinate, create corresponding node
-                ele_node = Node(coordinate=element, parent=node,
-                                G=gcost(node, element), H=heuristic(element, goal))
-                open_list.append(ele_node)
-        open_list.remove(node)
-        close_list.append(node)
-        open_list.sort(key=lambda x: x.F)
-    return open_list, close_list
+def path_update(open_heap, node_dict, close_set, current_node, target_coord, points, nn):
+    neighbours = neighbourSearch(current_node, nn, points)
+    for n_idx in neighbours:
+        neighbour_coord = points[n_idx]
+        neighbour_node = Node(coordinate=neighbour_coord, 
+                              H=heuristic(neighbour_coord, target_coord),
+                              parent=current_node)
+        neighbour_node.G = gCost(current_node, neighbour_node)
 
-def node_to_coordinate(node_list):
-    coordinate_list = [node.coordinate for node in node_list]
-    return coordinate_list
+        if tuple(neighbour_coord) in close_set:
+            continue
 
-def check_node_coincide(close_ls1, close_ls2):
-    cl1 = node_to_coordinate(close_ls1)
-    cl2 = node_to_coordinate(close_ls2)
-    intersect_ls = [node for node in cl1 if any(np.array_equal(node, other_node) for other_node in cl2)]
-    return intersect_ls
+        if tuple(neighbour_coord) not in node_dict or \
+            neighbour_node.G < node_dict[tuple(neighbour_node.coordinate)].G:
+            heapq.heappush(open_heap, neighbour_node)
+            node_dict[tuple(neighbour_coord)] = neighbour_node
+    return open_heap, node_dict, close_set
 
-def get_path(org_list, goal_list, coordinate):
-    pass
-
-def draw_control(org_closed, goal_closed, flag, start, end):
-    """
-    control the plot process, evaluate if the searching finished
-    flag == 0 : draw the searching process and plot path
-    flag == 1 or 2 : start or end is blocked, draw the border line
-    """
-    stop_loop = 0  # stop sign for the searching
-    org_closed_ls = node_to_coordinate(org_closed)
-    org_array = np.array(org_closed_ls)
-    goal_closed_ls = node_to_coordinate(goal_closed)
-    goal_array = np.array(goal_closed_ls)
-    path = None
-    # if show_animation:  # draw the searching process
-    #     draw(org_array, goal_array, start, end, bound)
-    if flag == 0:
-        node_intersect = check_node_coincide(org_closed, goal_closed)
-        if node_intersect:  # a path is find
-            path = get_path(org_closed, goal_closed, node_intersect[0])
-            stop_loop = 1
-            print('Path found!')
-    elif flag == 1:  # start point blocked first
-        stop_loop = 1
-        print('There is no path to the goal! Start point is blocked!')
-    elif flag == 2:  # end point blocked first
-        stop_loop = 1
-        print('There is no path to the goal! End point is blocked!')
-    return stop_loop, path
-
-def searching_control(start, end, nn):
-    # initial origin node and end node
+def bidirectional_a_star(start, end, nn, points):
     origin = Node(coordinate=start, H=heuristic(start, end))
     goal = Node(coordinate=end, H=heuristic(end, start))
-    # list for searching from origin to goal
-    origin_open: list = [origin]
-    origin_close: list = []
-    # list for searching from goal to origin
-    goal_open = [goal]
-    goal_close: list = []
-    # initial target
-    target_goal = end
-    # flag = 0 (not blocked) 1 (start point blocked) 2 (end point blocked)
-    flag = 0
-    path = None
-    while True:
-        # searching from start to end
-        origin_open, origin_close = \
-            find_path(origin_open, origin_close, target_goal, nn)
-        if not origin_open:     # no path condition
-            flag = 1    # origin node is blocked
-            break
-        # update target for searching from end to start
-        target_origin = min(origin_open, key=lambda x: x.F).coordinate
 
-        # searching from end to start
-        goal_open, goal_close = \
-            find_path(goal_open, goal_close, target_origin, nn)
-        if not goal_open:     # no path condition
-            flag = 2    # goal node is blocked
-            break
-        # update target for searching from start to end
-        target_goal = min(goal_open, key=lambda x: x.F).coordinate
-    
-        # continue searching
-        stop_sign, path = draw_control(origin_close, goal_close, flag, start, end)
-        if stop_sign:
-            break
-    return path
+    # from start to goal
+    origin_open_heap = []
+    heapq.heappush(origin_open_heap, origin)
+    origin_dict = {}
+    origin_dict[tuple(origin.coordinate)] = origin
+    origin_close = set()
 
+    # from goal to start
+    goal_open_heap = []
+    heapq.heappush(goal_open_heap, goal)
+    goal_dict = {}
+    goal_dict[tuple(goal.coordinate)] = goal
+    goal_close = set()
+
+    while origin_open_heap and goal_open_heap:
+        # from start to goal
+        forward_current_node = heapq.heappop(origin_open_heap)
+        forward_current_coord = tuple(forward_current_node.coordinate)
+
+        if forward_current_coord in origin_dict and \
+            forward_current_node.G <= origin_dict[forward_current_coord].G:
+        
+            origin_close.add(forward_current_coord)
+
+            if forward_current_coord in goal_close:
+                backward_node = goal_dict[forward_current_coord]
+                return reconstructPath(forward_current_node, backward_node, origin, goal)
+            
+            origin_open_heap, origin_dict, origin_close = path_update(origin_open_heap, origin_dict,
+                                                                      origin_close, forward_current_node,
+                                                                      goal.coordinate, points, nn)
+
+        # from goal to start
+        backward_current_node = heapq.heappop(goal_open_heap)
+        backward_current_coord = tuple(backward_current_node.coordinate)
+
+        if backward_current_coord in goal_dict and \
+            backward_current_node.G <= goal_dict[backward_current_coord].G:
+        
+            goal_close.add(backward_current_coord)
+
+            if backward_current_coord in origin_close:
+                forward_node = origin_dict[backward_current_coord]
+                return reconstructPath(forward_node, backward_current_node, origin, goal)
+        
+            goal_open_heap, goal_dict, goal_close = path_update(goal_open_heap, goal_dict,
+                                                                goal_close, backward_current_node,
+                                                                origin.coordinate, points, nn)
+    return None
 
 
 def main():
@@ -166,18 +129,27 @@ def main():
     start = points[0] 
     end = points[-1]
 
-    nn = NearestNeighbors(n_neighbors=3, algorithm='kd_tree').fit(points) 
-    path = searching_control(start, end, nn)
+    start_time = time.time()
+    nn = NearestNeighbors(n_neighbors=100, algorithm='kd_tree').fit(points) 
+    path = bidirectional_a_star(start, end, nn, points)
+    end_time = time.time()
+    print(f'Execution time: {end_time - start_time}')
 
-    # Plotting the point cloud
-    # plt.figure(figsize=(9, 16))
-    # plt.scatter(points[:, 0], points[:, 1], color='blue', marker='o', label='Point Cloud')
-    # plt.title("A* Pathfinding")
-    # plt.xlabel("X-axis")
-    # plt.ylabel("Y-axis")
-    # plt.legend()
-    # plt.grid(True)
-    # plt.show()
+    if path:
+        # Converting the path into a format suitable for plotting
+        path_points = np.array(path)
+
+        plt.figure(figsize=(9, 16))
+        plt.scatter(points[:, 0], points[:, 1], color='blue', marker='o', label='Point Cloud')
+        plt.plot(path_points[:, 0], path_points[:, 1], color='red', linewidth=2, label='Path')
+        plt.title("A* Pathfinding")
+        plt.xlabel("X-axis")
+        plt.ylabel("Y-axis")
+        plt.legend()
+        plt.grid(True)
+        plt.show()
+
+
 
 if __name__ == "__main__":
-    main()
+    main() 
