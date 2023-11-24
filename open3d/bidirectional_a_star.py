fix rotation matrix

open3d/bidirectional_a_star.py
@@ -152,10 +152,10 @@ def main():
     # points[-1] = np.array([2000,2000,2000])
 
     file_path = os.path.expanduser('~/Documents/Turtleterrain/src/turtleterrain/open3d/Images/')
-    points = np.loadtxt(file_path + 'processed_pcd_3.xyz')
+    points = np.loadtxt(file_path + 'processed_pcd_start.xyz')
     
-    start = np.array([0.28369, 0.49533, 0.0])
-    end = np.array([0.33822, 2.97588, -0.225])
+    start = np.array([0.0, 0.0, 0.0])
+    end = np.array([ 0.29968545,  2.66602373, -0.2 ])
     points = np.append(points, [start], axis=0)
     points = np.append(points, [end], axis=0)
 

open3d/terrain_cloud_processing.py
@@ -50,9 +50,13 @@ def ProcessCloud(np_pcd, robot_position, robot_orientation, target_coord):
     tensor_pcd = o3d.t.geometry.PointCloud.from_legacy(pcd.to_legacy())
     pcd_gpu = tensor_pcd.to(device=o3d.core.Device("cuda:0"))
 
+    o3d.visualization.draw_geometries([pcd_gpu.to_legacy()])
+
     # point cloud pre-processing    
     downpcd = pcd_gpu.voxel_down_sample(voxel_size=0.025)
     downpcd.estimate_normals(max_nn=30, radius=0.1)
+
+    o3d.visualization.draw_geometries([downpcd.to_legacy()])
 
     # fit plane
     inlier_cloud, boundary_cloud = fitPlane(downpcd)
@@ -83,17 +87,18 @@ def ProcessCloud(np_pcd, robot_position, robot_orientation, target_coord):
 
 if __name__ == "__main__":
     file_path = os.path.expanduser('~/Documents/Turtleterrain/src/turtleterrain/open3d/Images/')
-    np_pcd = np.loadtxt(file_path + 'flat_pothole_start.xyz')
+    np_pcd = np.loadtxt(file_path + 'flat_pothole_1.xyz')
 
-    robot_position = np.array([0.00699, -0.03745, 0.0])
-    xx = 0.0
+    robot_position = np.array([0.0, 0.0, 0.0])
+    xx = 1.0
     xy = 0.0
     robot_orientation = np.array([[xx, xy, 0], [-xy, xx, 0], [0, 0, 1]])
+    print(f'robot orientation: \n{robot_orientation}')
     robot = o3d.geometry.PointCloud()
     robot.points = o3d.utility.Vector3dVector([robot_position])
     robot.paint_uniform_color([0, 0, 1])
 
-    target_position = np.array([1.0, 5.0, 0.0])
+    target_position = np.array([0.5, 4.0, 0.0])
     
     plane_cloud, output_cloud, nearest_point = ProcessCloud(np_pcd, robot_position, robot_orientation, target_position)
 
@@ -115,6 +120,7 @@ if __name__ == "__main__":
     nearest_point_pcd.paint_uniform_color([0, 1, 0])
     
     o3d.io.write_point_cloud(file_path + 'processed_pcd_start.xyz', output_cloud_pcd)
+    print(f'start point: {robot_position}')
     print(f'nearest point: {nearest_point}')
     o3d.visualization.draw_geometries([transformed_cloud, output_cloud_pcd, robot, nearest_point_pcd])
 

scripts/cloud_processing.py
@@ -49,10 +49,14 @@ def ProcessCloud(np_pcd, robot_position, robot_orientation, target_coord):
     tensor_pcd = o3d.t.geometry.PointCloud.from_legacy(pcd.to_legacy())
     pcd_gpu = tensor_pcd.to(device=o3d.core.Device("cuda:0"))
 
+    # o3d.visualization.draw_geometries([pcd_gpu.to_legacy()])
+
     # point cloud pre-processing    
-    downpcd = pcd_gpu.voxel_down_sample(voxel_size=0.025)
+    downpcd = pcd_gpu.voxel_down_sample(voxel_size=0.01)
     downpcd.estimate_normals(max_nn=30, radius=0.1)
 
+    # o3d.visualization.draw_geometries([downpcd.to_legacy()])
+    
     # fit plane
     inlier_cloud, boundary_cloud = fitPlane(downpcd)
 
@@ -64,7 +68,7 @@ def ProcessCloud(np_pcd, robot_position, robot_orientation, target_coord):
 
     filtered_bound = torch.ones(inlier_tensor.shape[0], dtype=torch.bool, device='cuda:0')
     for boundary_pt in boundary_tensor:
-        filtered_bound = torch.logical_and(filtered_bound, filter_cloud(boundary_pt, inlier_tensor, 0.5))
+        filtered_bound = torch.logical_and(filtered_bound, filter_cloud(boundary_pt, inlier_tensor, 0.1))
     filtered_inlier_cloud = inlier_tensor[filtered_bound]
 
     # transfrom pcd to map frame
@@ -72,9 +76,28 @@ def ProcessCloud(np_pcd, robot_position, robot_orientation, target_coord):
 
     # find nearest point to target
     np_inlier_cloud = transformed_cloud.cpu().numpy()
-    transformed_cloud = o3d.geometry.PointCloud()
-    transformed_cloud.points = o3d.utility.Vector3dVector(np_inlier_cloud)
+    transformed_inlier_cloud = o3d.geometry.PointCloud()
+    transformed_inlier_cloud.points = o3d.utility.Vector3dVector(np_inlier_cloud)
 
-    nearest_point = nearest_search(transformed_cloud, target_coord)
+    nearest_point = nearest_search(transformed_inlier_cloud, target_coord)
+
+    # Visualizations
+    robot = o3d.geometry.PointCloud()
+    robot.points = o3d.utility.Vector3dVector([robot_position])
+    robot.paint_uniform_color([0, 0, 1])
+
+    transformed_inlier_cloud.paint_uniform_color([1, 0, 0])
+
+    nearest_point_pcd = o3d.geometry.PointCloud()
+    nearest_point_pcd.points = o3d.utility.Vector3dVector([nearest_point])    
+    nearest_point_pcd.paint_uniform_color([0, 1, 0])
+    
+    target_pcd = o3d.geometry.PointCloud()
+    target_pcd.points = o3d.utility.Vector3dVector([target_coord])
+    target_pcd.paint_uniform_color([1, 0, 1])
+
+    # o3d.visualization.draw_geometries([transformed_inlier_cloud, robot, nearest_point_pcd, target_pcd])
+
+
 
     return np_inlier_cloud, nearest_point

scripts/path_planner_node.py
@@ -14,6 +14,7 @@ from cloud_processing import ProcessCloud
 class O3DNode(Node):
     def __init__(self):
         super().__init__('open3d_node')
+        self.goal_reached = False
         self.target_pose = None
         self.amcl_pose = None
         self.current_position = None
@@ -80,8 +81,8 @@ class O3DNode(Node):
         r21 = 2*y*z + 2*x*w
         r22 = 1 - 2*x*x - 2*y*y
 
-        self.current_orientation = np.array([[r00, r01, r02],
-                                             [r10, r11, r12],
+        self.current_orientation = np.array([[-r00, -r01, r02],
+                                             [-r10, -r11, r12],
                                              [r20, r21, r22]])
         self.current_position = np.array(self.amcl_pose[:3])
         
@@ -112,8 +113,8 @@ class O3DNode(Node):
 
         if self.wp_counter % self.wp_interval == 0:
             # self.get_logger().info(f'target_pose: {self.target_pose}, current_pose: {self.current_position}')
-            # self.get_logger().info(f'current position: \n{self.current_position}')
-            # self.get_logger().info(f'current orientation: \n{self.current_orientation}')
+            self.get_logger().info(f'current position: \n{self.current_position}')
+            self.get_logger().info(f'current orientation: \n{self.current_orientation}')
 
             pcd_array = self.reshape_pcd(msg)
             pcd, nearest_point = ProcessCloud(pcd_array, self.current_position, self.current_orientation, self.target_pose)

scripts/path_planning.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 import time
 import numpy as np
+import matplotlib.pyplot as plt
+import open3d as o3d
 import heapq
 from sklearn.neighbors import NearestNeighbors
 from scipy.interpolate import CubicSpline
@@ -136,13 +138,49 @@ def cubicSplineSmoother(path, num_points):
     return waypoints
 
 def PathPlanner(points, start, end):
-    start_time = time.time()
     nn = NearestNeighbors(n_neighbors=100, algorithm='kd_tree').fit(points) 
     path = bidirectional_a_star(start, end, nn, points)
-    end_time = time.time()
-    # print(f'Execution time: {end_time - start_time}')
 
-    # return path
+
+    # Visualization
+    # path_points = np.array(path)
+    # fig = plt.figure(figsize=(9, 9))
+    # ax = fig.add_subplot(111, projection='3d')
+    # ax.scatter(points[:, 0], points[:, 1], points[:, 2], color='blue', marker='o', label='Point Cloud')
+    # ax.plot(path_points[:, 0], path_points[:, 1], path_points[:, 2], color='red', linewidth=2, label='Path')
+    # ax.plot(waypoints[:, 0], waypoints[:, 1], waypoints[:, 2], color='green', linewidth=2, label='Cubic Path')
+    # ax.set_title("3D A* Pathfinding")
+    # ax.set_xlabel("X-axis")
+    # ax.set_ylabel("Y-axis")
+    # ax.set_zlabel("Z-axis")
+    # ax.legend()
+    # ax.grid(True)
+    # plt.show()
+
+    robot = o3d.geometry.PointCloud()
+    robot.points = o3d.utility.Vector3dVector([start])
+    robot.paint_uniform_color([0, 0, 1])
+
+    points_pcd = o3d.geometry.PointCloud()
+    points_pcd.points = o3d.utility.Vector3dVector(points)
+    points_pcd.paint_uniform_color([1, 0, 0])
+
+    nearest_point_pcd = o3d.geometry.PointCloud()
+    nearest_point_pcd.points = o3d.utility.Vector3dVector([end])    
+    nearest_point_pcd.paint_uniform_color([0, 1, 0])
+    
+    target_pcd = o3d.geometry.PointCloud()
+    target_pcd.points = o3d.utility.Vector3dVector([[1.0, 3.0, 0.0]])
+    target_pcd.paint_uniform_color([1, 0, 1])
+
+    path_pcd = o3d.geometry.PointCloud()
+    path_pcd.points = o3d.utility.Vector3dVector(path)
+    
+    # o3d.visualization.draw_geometries([points_pcd, path_pcd, robot, nearest_point_pcd, target_pcd])
+
+
+
+
 
     num_points = 10
     waypoints = cubicSplineSmoother(path, num_points)